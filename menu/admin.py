efb713458c7c8e8e6edb8eaf518caaa95a8a047b
Dima <pixel.art@bk.ru>
2023-04-12 22:31:31 +0300

    tree-like menus
    Tree-like menus

--- menu/admin.py
+++ menu/admin.py
@@ -1,4 +1,16 @@
 from django.contrib import admin
 
+from .models import Menu, Submenu
+
 
 # Register your models here.
+admin.site.register(Menu)
+# class MenuAdmin(admin.ModelAdmin):
+#     model = Menu
+#     list_display = ['name', submenus]
+    
+#     @admin.display(description='All related to this menu submenus')
+#     def submenus(self, obj):
+#         return 
+    
+admin.site.register(Submenu)
